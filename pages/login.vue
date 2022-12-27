91393b5caddc0020a7f76f70fadd42885cd295f9
386083520 <386083520@qq.com>
2022-12-27 13:33:12 +0800

    登陆表单样式

--- pages/login.vue
+++ pages/login.vue
@@ -4,7 +4,23 @@
 			<image src="/static/logo.png" style="width: 50px;height: 50px"></image>
 			<text class="title">若依移动端登陆</text>
 		</view>
-		<view></view>
+		<view class="login-form-content">
+			<view class="input-item flex align-center">
+				<view class="iconfont icon-user icon"></view>
+				<input placeholder="请输入账号" class="input" maxlength="30"/>
+			</view>
+			<view class="input-item flex align-center">
+				<view class="iconfont icon-password icon"></view>
+				<input placeholder="请输入密码" class="input" type="password" maxlength="20"/>
+			</view>
+			<view class="input-item flex align-center">
+				<view class="iconfont icon-code icon"></view>
+				<input placeholder="请输入验证码" class="input" maxlength="4"/>
+			</view>
+			<view class="action-btn">
+				<button class="login-btn bg-blue">登陆</button>
+			</view>
+		</view>
 		<view></view>
 	</view>
 </template>
@@ -28,6 +44,30 @@
 		padding-top: 15%;
 		.title {
 			margin-left: 10px;
+		}
+	}
+	.login-form-content {
+		width: 80%;
+		margin: 20px auto;
+		margin-top: 15%;
+		.input-item {
+			border-radius: 20px;
+			background-color: #f5f6f7;
+			height: 45px;
+			margin: 20px auto;
+			.icon {
+				margin-left: 10px;
+				color: #999;
+			}
+			.input {
+				padding-left: 15px;
+				width: 100%;
+				font-size: 14px;
+			}
+		}
+		.login-btn {
+			margin-top: 40px;
+			height: 45px;
 		}
 	}
 }
