87d9bf07667f63d4dc4671078a104f9d32ad2919
386083520 <386083520@qq.com>
2022-12-27 12:24:57 +0800

    logo部分样式

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,11 @@
 <template>
-	<view>
-		login
+	<view class="normal-login-container">
+		<view class="logo-content flex align-center justify-center">
+			<image src="/static/logo.png" style="width: 50px;height: 50px"></image>
+			<text class="title">若依移动端登陆</text>
+		</view>
+		<view></view>
+		<view></view>
 	</view>
 </template>
 
@@ -8,12 +13,22 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.normal-login-container {
+	width: 100%;
+	.logo-content {
+		width: 100%;
+		font-size: 21px;
+		padding-top: 15%;
+		.title {
+			margin-left: 10px;
+		}
+	}
+}
 </style>
