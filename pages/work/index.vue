eed3ceb15641fc6980c6bf268eeb96be944876e9
386083520 <386083520@qq.com>
2022-12-28 14:19:37 +0800

    swiper

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,15 +1,42 @@
 <template>
 	<view>
-		work
+		<uni-swiper-dot :info="bannerData" :current="current">
+			<swiper @change="changeSwiper">
+				<swiper-item v-for="(item,index) in bannerData" :key="index">
+					<view>
+						<image :src="item.image"></image>
+					</view>
+				</swiper-item>
+			</swiper>
+		</uni-swiper-dot>
 	</view>
 </template>
 
 <script>
+	import UniSwiperDot from "../../uni_modules/uni-swiper-dot/components/uni-swiper-dot/uni-swiper-dot";
 	export default {
+		components: {UniSwiperDot},
 		data() {
 			return {
-				
+				current: 0,
+				bannerData: [
+					{
+						image: '/static/images/banner/banner01.jpg'
+					},
+					{
+						image: '/static/images/banner/banner02.jpg'
+					},
+					{
+						image: '/static/images/banner/banner03.jpg'
+					}
+				]
 			};
+		},
+		methods: {
+			changeSwiper(e) {
+				console.log('gsde', e)
+				this.current = e.detail.current
+			}
 		}
 	}
 </script>
