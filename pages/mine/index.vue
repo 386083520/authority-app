87ef7f6931f1c8b0e416176423bf265c6fd63b44
386083520 <386083520@qq.com>
2022-12-28 17:01:54 +0800

    用户信息样式

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -2,13 +2,15 @@
 	<view class="mine-container">
 		<view class="header-section">
 			<view class="flex justify-between">
-				<view>
-					<image :src="avatar"></image>
-					<view>
-						用户名：{{ name }}
+				<view class="flex align-center">
+					<image :src="avatar" class="cu-avatar xl round"></image>
+					<view class="user-info">
+						<view class="u_title">
+							用户名：{{ name }}
+						</view>
 					</view>
 				</view>
-				<view>
+				<view class="flex align-center">
 					<text>个人信息</text>
 					<view class="iconfont icon-right"></view>
 				</view>
@@ -29,5 +31,20 @@
 </script>
 
 <style lang="scss">
-
+.mine-container {
+	.header-section {
+		background-color: #3c96f3;
+		padding: 15px 15px 45px 15px;
+		color: #fff;
+		.cu-avatar {
+			border: 2px solid #eaeaea;
+		}
+		.user-info {
+			margin-left: 15px;
+			.u_title {
+				font-size: 18px;
+			}
+		}
+	}
+}
 </style>
